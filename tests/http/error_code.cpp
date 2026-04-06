7b260f3f2777af63800f4acd1ddb05959667e72d
annihilatorq <143023834+annihilatorq@users.noreply.github.com>
2026-04-06 15:58:32 +0200

    test(http): add tests for new error categories

--- tests/http/error_code.cpp
+++ tests/http/error_code.cpp
@@ -8,11 +8,29 @@ namespace {
 
   namespace error = aero::http::error;
 
+  using error::connection_error;
+  using error::connection_error_category;
+  using error::header_error;
+  using error::header_error_category;
   using error::protocol_error;
   using error::protocol_error_category;
+  using error::uri_error;
+  using error::uri_error_category;
 
 } // namespace
 
+TEST(HttpErrorCodes, AllHeaderErrorsHaveMessage) {
+  aero::tests::test_enum_error_code_messages<header_error>(header_error_category());
+}
+
 TEST(HttpErrorCodes, AllProtocolErrorsHaveMessage) {
   aero::tests::test_enum_error_code_messages<protocol_error>(protocol_error_category());
+}
+
+TEST(HttpErrorCodes, AllUriErrorsHaveMessage) {
+  aero::tests::test_enum_error_code_messages<uri_error>(uri_error_category());
+}
+
+TEST(HttpErrorCodes, AllConnectionErrorsHaveMessage) {
+  aero::tests::test_enum_error_code_messages<connection_error>(connection_error_category());
 }
