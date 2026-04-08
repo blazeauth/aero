51d85ad69d72333012f970b80dac65ecc8ef4b3e
annihilatorq <143023834+annihilatorq@users.noreply.github.com>
2026-04-08 11:48:43 +0200

    fix: add missing includes to `aero/http.hpp`

--- include/aero/http.hpp
+++ include/aero/http.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include "aero/http/client.hpp"
+#include "aero/http/client_options.hpp"
 #include "aero/http/error.hpp"
 #include "aero/http/headers.hpp"
 #include "aero/http/method.hpp"
+#include "aero/http/port.hpp"
 #include "aero/http/request.hpp"
 #include "aero/http/request_line.hpp"
 #include "aero/http/response.hpp"
 #include "aero/http/status_code.hpp"
 #include "aero/http/status_line.hpp"
+#include "aero/http/uri.hpp"
 #include "aero/http/version.hpp"
